298ce4bdec5e415389bfaec5fcb55d0f14803e3b
Rodrigo <rwebler@gmail.com>
2016-07-23 11:23:21 -0300

    Problem navigation done

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -35,4 +35,18 @@ describe('LearnJS', function() {
       expect(view.find('code').text().trim()).toEqual('function problem() { return __; }');
     });
   });
+  describe('answer section', function() {
+    it('can check a correct answer by hitting a button', function() {
+      var view = learnjs.problemView('1');
+      view.find('.answer').val('true');
+      view.find('.check-btn').click();
+      expect(view.find('.result span').text()).toEqual('Correct!');
+    });
+    it('rejects an incorrect answer', function() {
+      var view = learnjs.problemView('1');
+      view.find('.answer').val('false');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Incorrect!');
+    });
+  });
 });
